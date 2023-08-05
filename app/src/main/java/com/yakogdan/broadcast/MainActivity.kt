08ea8386950df6237aa980325178360cd14c66ae
yakogdan <yakogdan@gmail.com>
2023-08-05 15:21:03 +0800

    Передача параметров в броадкаст-сообщениях

--- app/src/main/java/com/yakogdan/broadcast/MainActivity.kt
+++ app/src/main/java/com/yakogdan/broadcast/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.yakogdan.broadcast
 
 import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private val receiver = MyReceiver()
+    private lateinit var progressBar: ProgressBar
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(p0: Context?, intent: Intent?) {
+            if (intent?.action == "loaded") {
+                val percent = intent.getIntExtra("percent", 0)
+                progressBar.progress = percent
+            }
+        }
+    }
 
     private var count = 0
 
@@ -17,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progressBar = findViewById(R.id.progressBar)
         findViewById<Button>(R.id.button).setOnClickListener {
             Intent(MyReceiver.ACTION_CLICKED).apply {
                 putExtra(MyReceiver.EXTRA_COUNT, ++count)
@@ -24,11 +37,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
         val intentFilter = IntentFilter().apply {
-            addAction(Intent.ACTION_BATTERY_LOW)
-            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-            addAction(MyReceiver.ACTION_CLICKED)
+            addAction("loaded")
         }
         registerReceiver(receiver, intentFilter)
+        Intent(this, MyService::class.java).apply {
+            startService(this)
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/yakogdan/broadcast/MyReceiver.kt
+++ app/src/main/java/com/yakogdan/broadcast/MyReceiver.kt
@@ -8,6 +8,10 @@ import android.widget.Toast
 class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         when (intent?.action) {
+            "loaded" -> {
+                val percent = intent.getIntExtra("percent", 0)
+                Toast.makeText(context, "Loaded $percent%", Toast.LENGTH_SHORT).show()
+            }
             ACTION_CLICKED -> {
                 val count = intent.getIntExtra(EXTRA_COUNT, 0)
                 Toast.makeText(context, "Clicked $count", Toast.LENGTH_SHORT).show()
