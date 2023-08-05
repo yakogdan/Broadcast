5d1424794c650f4931b9ceb04565ce74efb631de
yakogdan <yakogdan@gmail.com>
2023-08-05 13:52:29 +0800

    Создание кастомного ресивера

--- app/src/main/java/com/yakogdan/broadcast/MainActivity.kt
+++ app/src/main/java/com/yakogdan/broadcast/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.yakogdan.broadcast
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private val receiver = MyReceiver()
 
+    private var count = 0
+
+    @SuppressLint("UnspecifiedRegisterReceiverFlag")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            Intent(MyReceiver.ACTION_CLICKED).apply {
+                putExtra(MyReceiver.EXTRA_COUNT, ++count)
+                sendBroadcast(this)
+            }
+        }
         val intentFilter = IntentFilter().apply {
             addAction(Intent.ACTION_BATTERY_LOW)
             addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+            addAction(MyReceiver.ACTION_CLICKED)
         }
         registerReceiver(receiver, intentFilter)
     }
--- app/src/main/java/com/yakogdan/broadcast/MyReceiver.kt
+++ app/src/main/java/com/yakogdan/broadcast/MyReceiver.kt
@@ -8,6 +8,11 @@ import android.widget.Toast
 class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         when (intent?.action) {
+            ACTION_CLICKED -> {
+                val count = intent.getIntExtra(EXTRA_COUNT, 0)
+                Toast.makeText(context, "Clicked $count", Toast.LENGTH_SHORT).show()
+            }
+
             Intent.ACTION_BATTERY_LOW -> {
                 Toast.makeText(context, "ACTION_BATTERY_LOW", Toast.LENGTH_SHORT).show()
             }
@@ -21,5 +26,10 @@ class MyReceiver : BroadcastReceiver() {
                 ).show()
             }
         }
+    }
+
+    companion object {
+        const val ACTION_CLICKED = "clicked"
+        const val EXTRA_COUNT = "count"
     }
 }
