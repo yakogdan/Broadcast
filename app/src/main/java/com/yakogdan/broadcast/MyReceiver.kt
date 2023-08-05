e20a9ced7ab377847aca4b547d87f49dac41e773
yakogdan <yakogdan@gmail.com>
2023-08-05 13:36:27 +0800

    Обработка нескольких сообщений.

--- app/src/main/java/com/yakogdan/broadcast/MyReceiver.kt
+++ app/src/main/java/com/yakogdan/broadcast/MyReceiver.kt
@@ -5,11 +5,20 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 
-class MyReceiver: BroadcastReceiver() {
+class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         when (intent?.action) {
             Intent.ACTION_BATTERY_LOW -> {
                 Toast.makeText(context, "ACTION_BATTERY_LOW", Toast.LENGTH_SHORT).show()
+            }
+
+            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
+                val turnedOn = intent.getBooleanExtra("state", false)
+                Toast.makeText(
+                    context,
+                    "Airplane mode changed. Turned on: $turnedOn",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
