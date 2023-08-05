f5e51fa9ee6262559b2d6557f72803771ec217c0
yakogdan <yakogdan@gmail.com>
2023-08-05 12:52:22 +0800

    Создание и регистрация ресиверов

---
+++ app/src/main/java/com/yakogdan/broadcast/MyReceiver.kt
@@ -0,0 +1,16 @@
+package com.yakogdan.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when (intent?.action) {
+            Intent.ACTION_BATTERY_LOW -> {
+                Toast.makeText(context, "ACTION_BATTERY_LOW", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
